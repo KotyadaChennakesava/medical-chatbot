27f7d817687aa6b90d5cd762f03e42c0cff85278
KotyadaChennakesava <chennakesavakotyada235@gmail.com>
2025-09-16 15:40:31 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical chatbot",
+    version="0.1.0",
+    author="Steve harrington",
+    author_email="chennakesavakotyada235@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
